f3bf2e82f1a9b522e0b1cc10f01820b1d8d13b6a
AhsanulHaq777 <66363908+AhsanulHaq777@users.noreply.github.com>
2020-07-14 23:15:08 +0500

    created Navbar.js and added to Myroutes.js

---
+++ src/Components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import { yellow } from '@material-ui/core/colors';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export default function NavBar() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h5" className={classes.title}>
+                        Shoes Store
+                    </Typography>
+                    <div>
+                        <Link to="/" >Home</Link> {' | '}
+                        <Link to="Shoes" >Shoes</Link> {' | '}
+                        <Link to="about" >About</Link>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Routes, Route} from 'react-router';
 import Home from './Components/Home';
+import NavBar from './Components/Navbar';
 
 function MyRouteConfig() {
     return(
         <div>
+            <NavBar />
             <Routes>
                 <Route path="/" element={<Home />}></Route>
             </Routes>
